6df084e10908bcb1757f984d02132939847c2e12
Raymond Carino <rcarino@classpass.com>
2017-02-07 14:51:57 -0500

    start hackathon project

--- src/app/Main.js
+++ src/app/Main.js
@@ -9,6 +9,7 @@ import {deepOrange500} from 'material-ui/styles/colors';
 import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TabsExampleSwipeable from './tabs'
 
 const styles = {
   container: {
@@ -55,23 +56,24 @@ class Main extends Component {
 
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
-        <div style={styles.container}>
-          <Dialog
-            open={this.state.open}
-            title="Super Secret Password"
-            actions={standardActions}
-            onRequestClose={this.handleRequestClose}
-          >
-            1-2-3-4-5
-          </Dialog>
-          <h1>Material-UI</h1>
-          <h2>example project</h2>
-          <RaisedButton
-            label="Super Secret Password"
-            secondary={true}
-            onTouchTap={this.handleTouchTap}
-          />
-        </div>
+        <TabsExampleSwipeable />
+        {/*<div style={styles.container}>*/}
+          {/*<Dialog*/}
+            {/*open={this.state.open}*/}
+            {/*title="Super Secret Password"*/}
+            {/*actions={standardActions}*/}
+            {/*onRequestClose={this.handleRequestClose}*/}
+          {/*>*/}
+            {/*1-2-3-4-5*/}
+          {/*</Dialog>*/}
+          {/*<h1>Material-UI</h1>*/}
+          {/*<h2>example project</h2>*/}
+          {/*<RaisedButton*/}
+            {/*label="Super Secret Password"*/}
+            {/*secondary={true}*/}
+            {/*onTouchTap={this.handleTouchTap}*/}
+          {/*/>*/}
+        {/*</div>*/}
       </MuiThemeProvider>
     );
   }
