77065fa5c286e94c67763038c0c3fddaf3c17ed2
Raymond Carino <rcarino@classpass.com>
2017-02-07 17:19:48 -0500

    unjankify search app bar

--- src/app/searchAppBar.js
+++ src/app/searchAppBar.js
@@ -1,38 +1,9 @@
 import React, {Component} from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import FlatButton from 'material-ui/FlatButton';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-
-class Login extends Component {
-    static muiName = 'FlatButton';
-
-    render() {
-        return (
-            <FlatButton {...this.props} label="Login" />
-        );
-    }
-}
-
-const Logged = (props) => (
-    <IconMenu
-        {...props}
-        iconButtonElement={
-            <IconButton><MoreVertIcon /></IconButton>
-        }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-    >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
-        <MenuItem primaryText="Sign out" />
-    </IconMenu>
-);
-
-Logged.muiName = 'IconMenu';
+import ContentFilter from 'material-ui/svg-icons/content/filter-list';
 
 /**
  * This example is taking advantage of the composability of the `AppBar`
@@ -63,8 +34,9 @@ class SearchAppBar extends Component {
         return (
             <div>
                 <AppBar
-                    iconElementLeft={sportFilter}
-                    iconElementRight={this.state.logged ? <Logged /> : <Login />}
+                    showMenuIconButton={false}
+                    title="All Sports"
+                    iconElementRight={<IconButton><ContentFilter /></IconButton>}
                 />
             </div>
         );
