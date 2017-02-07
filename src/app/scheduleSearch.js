877a6974ec72ad1a9e7176121434339b6d6c0594
Raymond Carino <rcarino@classpass.com>
2017-02-07 17:36:07 -0500

    more realistic list text

--- src/app/scheduleSearch.js
+++ src/app/scheduleSearch.js
@@ -32,132 +32,56 @@ const ListExampleMessages = () => (
     <div>
         <SearchAppBar />
             <List>
-                <Subheader>Today</Subheader>
                 <ListItem
-                    leftAvatar={<Avatar src="images/ok-128.jpg" />}
-                    primaryText="Brunch this weekend?"
+                    primaryText="Competitive Basketball"
                     secondaryText={
                         <p>
-                            <span style={{color: darkBlack}}>Brendan Lim</span> --
-                            I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                            <span style={{color: darkBlack}}>Wed, Feb 08 2017</span> --
+                            Podunk Community Center Court 2
                         </p>
                     }
                     secondaryTextLines={2}
                 />
-                <Divider inset={true} />
+                <Divider  />
                 <ListItem
-                    leftAvatar={<Avatar src="images/kolage-128.jpg" />}
-                    primaryText={
-                        <p>Summer BBQ&nbsp;&nbsp;<span style={{color: lightBlack}}>4</span></p>
-                    }
+                    primaryText="Intermediate Softball"
                     secondaryText={
                         <p>
-                            <span style={{color: darkBlack}}>to me, Scott, Jennifer</span> --
-                            Wish I could come, but I&apos;m out of town this weekend.
+                            <span style={{color: darkBlack}}>Wed, Feb 08 2017</span> --
+                            Podunk Community Center Court 2
                         </p>
                     }
                     secondaryTextLines={2}
                 />
-                <Divider inset={true} />
+                <Divider />
                 <ListItem
-                    leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
-                    primaryText="Oui oui"
+                    primaryText="Beginner Volleyball"
                     secondaryText={
                         <p>
-                            <span style={{color: darkBlack}}>Grace Ng</span> --
-                            Do you have Paris recommendations? Have you ever been?
+                            <span style={{color: darkBlack}}>Wed, Feb 08 2017</span> --
+                            Podunk Community Center Court 2
                         </p>
                     }
                     secondaryTextLines={2}
                 />
-                <Divider inset={true} />
+                <Divider />
                 <ListItem
-                    leftAvatar={<Avatar src="images/kerem-128.jpg" />}
-                    primaryText="Birdthday gift"
+                    primaryText="Beginner Dodgeball"
                     secondaryText={
                         <p>
-                            <span style={{color: darkBlack}}>Kerem Suer</span> --
-                            Do you have any ideas what we can get Heidi for her birthday? How about a pony?
+                            <span style={{color: darkBlack}}>Wed, Feb 08 2017</span> --
+                            Podunk Community Center Court 2
                         </p>
                     }
                     secondaryTextLines={2}
                 />
-                <Divider inset={true} />
+                <Divider />
                 <ListItem
-                    leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
-                    primaryText="Recipe to try"
+                    primaryText="Competitive Soccer"
                     secondaryText={
                         <p>
-                            <span style={{color: darkBlack}}>Raquel Parrado</span> --
-                            We should eat this: grated squash. Corn and tomatillo tacos.
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-            </List>
-
-            <List>
-                <Subheader>Today</Subheader>
-                <ListItem
-                    leftAvatar={<Avatar src="images/ok-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Brendan Lim"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Brunch this weekend?</span><br />
-                            I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/kolage-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="me, Scott, Jennifer"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Summer BBQ</span><br />
-                            Wish I could come, but I&apos;m out of town this weekend.
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Grace Ng"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Oui oui</span><br />
-                            Do you have any Paris recs? Have you ever been?
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/kerem-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Kerem Suer"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Birthday gift</span><br />
-                            Do you have any ideas what we can get Heidi for her birthday? How about a pony?
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Raquel Parrado"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Recipe to try</span><br />
-                            We should eat this: grated squash. Corn and tomatillo tacos.
+                            <span style={{color: darkBlack}}>Wed, Feb 08 2017</span> --
+                            Podunk Community Center Court 2
                         </p>
                     }
                     secondaryTextLines={2}
